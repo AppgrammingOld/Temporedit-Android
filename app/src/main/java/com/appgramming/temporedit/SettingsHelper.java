5466fecfb1c0cafa49c2baca054740f238dac195
Hello World Writer <writer@helloworldwriter.com>
2019-03-01 14:16:19 +0200

    Add settings helper

---
+++ app/src/main/java/com/appgramming/temporedit/SettingsHelper.java
@@ -0,0 +1,27 @@
+package com.appgramming.temporedit;
+
+import android.graphics.Typeface;
+
+class SettingsHelper {
+
+    static Typeface parseFontTypeface(String styleString) {
+        switch (styleString) {
+            case "DEFAULT": return Typeface.DEFAULT;
+            case "DEFAULT_BOLD": return Typeface.DEFAULT_BOLD;
+            case "MONOSPACE":return Typeface.MONOSPACE;
+            case "SANS_SERIF":return Typeface.SANS_SERIF;
+            case "SERIF":return Typeface.SERIF;
+            default: return Typeface.DEFAULT;
+        }
+    }
+
+    static int parseFontStyle(String styleString) {
+        switch (styleString) {
+            case "NORMAL": return Typeface.NORMAL;
+            case "BOLD": return Typeface.BOLD;
+            case "ITALIC": return Typeface.ITALIC;
+            case "BOLD_ITALIC": return Typeface.BOLD_ITALIC;
+            default: return Typeface.NORMAL;
+        }
+    }
+}
